0d8f4d2085977128a25b4eb25c799c3b865202d6
kim-seung-hun <https://github.com/kim-seung-hun/20220621>
2022-06-28 16:44:51 +0900

    2022.06.28 four

--- login/startPage/text.js
+++ login/startPage/text.js
@@ -1,10 +1,10 @@
 export class Text {
   constructor() {
     this.canvas = document.createElement("canvas");
-    this.canvas.style.position = "absolute";
-    this.canvas.style.left = "0";
-    this.canvas.style.top = "0";
-    document.body.appendChild(this.canvas);
+    // this.canvas.style.position = "absolute";
+    // this.canvas.style.left = "0";
+    // this.canvas.style.top = "0";
+    // document.body.appendChild(this.canvas);
 
     this.ctx = this.canvas.getContext("2d");
   }
@@ -15,13 +15,13 @@ export class Text {
 
     const myText = str;
     const fontWidth = 700;
-    const fontSize = 800;
+    const fontSize = 600;
     const fontName = "Hind";
 
     this.ctx.clearRect(0, 0, stageWidth, stageHeight);
     this.ctx.font = `${fontWidth} ${fontSize}px ${fontName}`;
     this.ctx.fillStyle = `rgba(0, 0, 0, 0.3)`;
-    this.ctx.textBaseline = `middle`;
+    this.ctx.textBaseline = `bottom`;
     const fontPos = this.ctx.measureText(myText);
     this.ctx.fillText(
       myText,
