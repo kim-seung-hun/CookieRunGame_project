f4e82c50cea8f6e15fae213a641d0f3be42fe8d2
onedayonecommit <rudghks0981@gmail.com>
2022-07-07 17:04:34 +0900

    password

--- cssandjs/js/login.js
+++ cssandjs/js/login.js
@@ -11,3 +11,5 @@ setInterval(() => {
     let second = Math.floor(livetime - (day * 86400) - (hour * 3600) - (minute * 60))
     document.querySelector('#livetime').innerHTML = `가입 하신지 ${day}일 ${hour}시간 ${minute}분 ${second}초 경과하였습니다.`
 }, 1000);
+
+//dfa
