f89cf55d134b06e360372a6c22876b7423b2fb9d
kim-seung-hun <rlatmdgns16@naver.com>
2022-07-07 10:16:15 +0900

    test

--- login/startPage/visual.js
+++ login/startPage/visual.js
@@ -11,7 +11,7 @@ export class Visual {
     this.mouse = {
       x: 0,
       y: 0,
-      radius: 30,
+      radius: 40,
     };
 
     document.addEventListener("pointermove", this.onMove.bind(this), false);
